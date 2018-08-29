29df537c8641d20c99c03b882c4b20918f9c088d
Natasha S Love <debora@inbox.ru>
2018-08-29 18:04:26 -0500

    Initial commit

---
+++ Message.java
@@ -0,0 +1,17 @@
+class Message {
+	//array of as many strings as need - method
+	public static void main (String [] args) {
+		//easy way to print in java
+		System.out.println(
+			"==========================================================================\n" +
+			"\n"+
+			"\t" + "\t" + "Natasha Love\n" +
+			"**************************************************************************" +
+			"\n"+
+			"\t" + "\t" + "Last week I learned :\n" + "\n"+
+			"\t" + "- How to create programs in JAVA, compile and run them;\n" +
+			"\t" + "- How to create a Java application that produces GUI output.\n" +			
+			"\n"+			
+			"==========================================================================\n");
+	}
+}
